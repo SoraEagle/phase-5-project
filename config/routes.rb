9b8d5296bd7f276d9d8cd82010531f0d6af7b4f1
Matthew Phillips <phillips.matthew829@gmail.com>
2022-11-30 14:31:30 -0600

    Add routes and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :binders, only: [:index, :create, :update]
+  resources :decks, only: [:index, :create, :update, :destroy]
+  resources :flashcards, only: [:index, :create, :update, :destroy]
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-  # post "signup", to: "users#create"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
