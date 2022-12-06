4932e6b36854487bdbc899d82ef72e65aa1f0d10
Matthew Phillips <phillips.matthew829@gmail.com>
2022-12-06 14:15:56 -0600

    Create store.js

---
+++ client/src/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import usersReducer from "./features/users/usersSlice";
+import bindersReducer from "./features/binders/bindersSlice";
+import decksReducer from "./features/decks/decksSlice";
+import flashcardsReducer from "./features/flashcards/flashcardsSlice";
+
+const store = configureStore({
+    reducer: {
+        users: usersReducer,
+        binders: bindersReducer,
+        decks: decksReducer,
+        flashcards: flashcardsReducer,
+    },
+});
+
+export default store;
