ce6fa5df8411790b302c7f4a271562a9ebbbad16
Matthew Phillips <phillips.matthew829@gmail.com>
2022-12-07 11:47:19 -0600

    Fix errors

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './store';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
