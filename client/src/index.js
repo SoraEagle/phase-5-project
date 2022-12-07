6dadfba99affa4d8c6511994ab051fba02877c59
Matthew Phillips <phillips.matthew829@gmail.com>
2022-12-07 11:11:40 -0600

    Fix index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import ThunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const composedEnhancer = composeWithDevTools(applyMiddleware(ThunkMiddleware));
-const store = createStore(catsReducer, composedEnhancer);
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
