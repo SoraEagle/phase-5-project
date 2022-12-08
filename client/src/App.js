b3a30c2e121ae4722cbb4144e9fc04989515c0a1
Matthew Phillips <phillips.matthew829@gmail.com>
2022-12-08 16:17:00 -0600

    Edit App.js and delete logos

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import NavBar from './components/Navigation/NavBar';
+import Footer from './components/Footer';
+import Login from './components/Authentication/Login';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if(r.ok) r.json().then((currentUser) => setCurrentUser(currentUser));
+    });
+  }, []);
+
+  if(!currentUser) return (
+    <Router>
+      <Login onLogin={setCurrentUser} errors={errors} setErrors={setErrors} isLoading={isLoading} setIsLoading={setIsLoading} />
+      <Footer />
+    </Router>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="App" className="App">
+      {currentUser ? <h1>Logged In!</h1> : null}
+      <NavBar setCurrentUser={setCurrentUser} setErrors={setErrors} />
+      <Routes>
+        <Route path={"/binders"} /* element={<Binders currentUser={currentUser} />} */ />
+      </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
