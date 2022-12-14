ba91b399041c18d6966f32501f694984ff4f9f88
Matthew Phillips <phillips.matthew829@gmail.com>
2022-12-14 15:16:49 -0600

    Fix relationship errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
 
     has_many :flashcards
     has_many :decks, through: :flashcards
-    has_many :binders, through: :flashcards, through: :decks
+    has_many :binders, through: :decks
 
     # Validations
-    validates :user, presence: true, uniqueness: true, length: {minimum: 5}, exclusion: {
+    validates :username, presence: true, uniqueness: true, length: {minimum: 5}, 
+    exclusion: {
         in: %w(+ - = _ @ npm), message: "%{value} is not allowed!"
     }
     validates :password, presence: true, length: {minimum: 4}
