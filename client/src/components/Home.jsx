a878a68e9dcccbe0d526146bd8515cfdb111f260
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-17 16:13:09 -0600

    Create Home.jsx

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home(){
+  return (
+    <header>
+        <div>
+            <section id='notice'>
+                <h2>Welcome to Cardlet!</h2>
+            </section>
+            <section>
+            <p id='text-left'>The purpose of this site is simple:</p>
+            </section>
+        </div>
+    </header>
+  )
+}
+
+export default Home;
