bbe9c9d0b362e28598e1348a03194aa05c4c8fed
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-18 16:07:56 -0600

    Remove unused code

--- client/src/features/binders/BindersContainer.jsx
+++ client/src/features/binders/BindersContainer.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import {useSelector, useDispatch} from "react-redux";
+import { useSelector } from "react-redux";
 import BinderInput from './BinderInput';
-import { binderAdded } from './bindersSlice';
+import Binders from './Binders';
 
 function BindersContainer(){
-  const dispatch = useDispatch();
   const binders = useSelector((state) => state.binders.binders);
-
-  function handleBinderSubmit(name){
-    dispatch(binderAdded(name));
-  }
   return (
     <div>
-      <BinderInput onBinderSubmit={handleBinderSubmit} />
-      <ul></ul> {/* Map out a list of the User's Binder objects, with each rendering as an image */}
+      <BinderInput />
+      <Binders binders={binders} />
     </div>
-  )
+  );
 }
 
 export default BindersContainer;
