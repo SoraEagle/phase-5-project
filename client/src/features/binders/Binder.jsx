2d9a245ab2aa1c6939e9f6b3cd66dce6eaa23489
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-24 14:03:19 -0600

    Add functionality to Binder

--- client/src/features/binders/Binder.jsx
+++ client/src/features/binders/Binder.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Binder({binder}){
+    const navigate = useNavigate();
+    
+    function handleClick(){
+        if(binder) navigate(`/binders/${binder.id}`)
+    }
     return (
-    <div>
-        <li>
-            {binder.name}
+    <div> {/* Clicking on either the <img> or text MUST navigate the User to an page that shows only that binder's decks */}
+        <li id='binder-name'> {/* Add an <img> that navigates to that binder's route */}
+            <div> {/* `/binders/${binder.id}/decks */}
+                <img id='binder-image' onClick={handleClick}
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR19DFLsrFz_7D9N_yGiBNugav84-FEsNjm3Q&usqp=CAU'
+                    alt={binder.name}
+                />
+                <Link to={`/binders/${binder.id}`}>{binder.name}</Link>
+            </div>
         </li>
     </div>
   );
