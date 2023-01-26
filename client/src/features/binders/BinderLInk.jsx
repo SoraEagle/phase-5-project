f62220d3b91c62d5eef35bc74396f383d32eebd1
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-25 19:15:45 -0600

    Remove unused code

--- client/src/features/binders/BinderLInk.jsx
+++ client/src/features/binders/BinderLInk.jsx
@@ -5,7 +5,6 @@ function BinderLink({binder}){
     return (
     <div id='binder'> {/* Clicking on either the <img> or text MUST navigate the User to an page that shows only that binder's decks */}
         <img id='binder-image' 
-            // onClick={handleClick}
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR19DFLsrFz_7D9N_yGiBNugav84-FEsNjm3Q&usqp=CAU'
             alt={binder.name}
         />
