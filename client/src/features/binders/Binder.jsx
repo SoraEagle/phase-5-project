5278e371b5b23f9134155dc56a65682d11cf75b0
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-25 19:13:51 -0600

    Edit Binder.jsx

--- client/src/features/binders/Binder.jsx
+++ client/src/features/binders/Binder.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBinders } from './bindersSlice';
+import DeckLink from '../decks/DeckLink';
+import DeckInput from '../decks/DeckInput';
 
 function Binder(){
+    const dispatch = useDispatch();
     const params = useParams();
-    const binders = useSelector((state) => state.binders.entities); // Find the Binder User is viewing!!!
+    const binders = useSelector((state) => state.binders.entities);
+    
+    useEffect(() => {
+      dispatch(fetchBinders());
+      // Grab the specific Binder object
+    }, [dispatch]);
 
-    console.log(params);
+    const myBinder = binders.filter(b => {
+      return (b.id.toString() === params.id);
+    })
+    console.log(myBinder);
+
+    binders.map((b) => {
+      console.log(b);
+    });
   return (
     <div>
-        Binder
+      {binders.map((b) => {
+        if(b.id.toString() === params.id){ // Check for the corresponding Binder
+          console.log("This Binder's Decks: ", b.decks);
+          return (
+              <div>
+                {/* <h3>{b.name}</h3>
+                <DeckInput binder={b} />
+                  <p key={b.decks}>{b.decks}</p>
+                  {(b.decks).map((deck) => {
+                    <DeckLink key={deck.id} deck={deck} />
+                  })} */}
+              </div>
+          );
+        }
+      })}
     </div>
   )
 }
