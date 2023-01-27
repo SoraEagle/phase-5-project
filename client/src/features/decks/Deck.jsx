87e8b5d4984f52c5da12d10e4b25227aa948ff07
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-27 16:21:11 -0600

    Add functionality to Deck.jsx

--- client/src/features/decks/Deck.jsx
+++ client/src/features/decks/Deck.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDecks } from './decksSlice';
 
 function Deck(){
+    const dispatch = useDispatch();
+    const params = useParams();
+    const decks = useSelector((state) => state.decks.entities);
+
+    useEffect(() => {
+        dispatch(fetchDecks());
+    }, [dispatch]);
   return (
-    <div>Deck</div>
+    <div>
+        Deck
+    </div>
   )
 }
 
