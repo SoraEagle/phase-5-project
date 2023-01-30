f9c4d44d236a8a59d890c07fe370e2dad7b1c0db
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-30 11:48:22 -0600

    Add dispatch, useEffect, and fetchBinders

--- client/src/features/binders/BindersContainer.jsx
+++ client/src/features/binders/BindersContainer.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch } from "react-redux";
+import { fetchBinders } from './bindersSlice';
 import BinderInput from './BinderInput';
 import Binders from './Binders';
 
 function BindersContainer(){
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBinders());
+  }, [dispatch]);
   return (
     <div>
       <BinderInput />
