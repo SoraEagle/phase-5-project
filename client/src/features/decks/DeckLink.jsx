833838d417093b5bcd2002ba00ebb379b20c89b9
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-30 16:20:58 -0600

    Add destination to DeckLink's Link

--- client/src/features/decks/DeckLink.jsx
+++ client/src/features/decks/DeckLink.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function DeckLink({deck}){
-    console.log(deck); // Only gets triggered if there is an existing Deck
+function DeckLink({binder, deck}){
   return (
-    <div id='deck'>
+    <div id='deck-link'>
         <img id='deck-image'
             src='https://media.istockphoto.com/id/172162468/photo/fan-of-colored-index-cards.jpg?s=612x612&w=0&k=20&c=6RHWhNpuq4bXu3dLCnNqk28a6WejUSTWG58ahIm8DL4='
             alt={deck.name}
          />
          <p>&nbsp;</p>
-         <Link id='linkStyles'>
+         <Link id='linkStyles' to={`/binders/${binder.id}/decks/${deck.id}`}>
             {deck.name}
          </Link>
     </div>
