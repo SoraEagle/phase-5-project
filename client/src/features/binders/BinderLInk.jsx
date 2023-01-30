ddb634c41cb5e977589251df48de5e734d6cba45
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-30 16:21:22 -0600

    Edit CSS

--- client/src/features/binders/BinderLInk.jsx
+++ client/src/features/binders/BinderLInk.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function BinderLink({binder}){
     return (
-    <div id='binder'> {/* Clicking on either the <img> or text MUST navigate the User to an page that shows only that binder's decks */}
+    <div id='binder-link'>
         <img id='binder-image' 
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR19DFLsrFz_7D9N_yGiBNugav84-FEsNjm3Q&usqp=CAU'
             alt={binder.name}
