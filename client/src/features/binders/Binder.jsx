fbdf9d07fa2351381d343a9afee9f7c27eb4c8ed
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-31 12:01:47 -0600

    Refactor component

--- client/src/features/binders/Binder.jsx
+++ client/src/features/binders/Binder.jsx
@@ -1,48 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchBinders } from './bindersSlice';
+import { useSelector } from 'react-redux';
 import DeckLink from '../decks/DeckLink';
 import DeckInput from '../decks/DeckInput';
 
 function Binder(){
-    const dispatch = useDispatch();
-    const params = useParams();
-    const binders = useSelector((state) => state.binders.entities);
-    // const [binder, setBinder] = useState(null);
-    
-    useEffect(() => {
-      dispatch(fetchBinders());
-      // Grab the specific Binder object
-    }, [dispatch]);
+  const params = useParams();
+  const binders = useSelector((state) => state.binders.entities);
 
-    function checkBinder(binder){
-      return binder.id.toString() === params.id;
-    }
-
-    binders.map((binder) => {
-      binder = binders.find(checkBinder); // Grabs the Binder object the User is viewing
-      console.log(binders.find(checkBinder));
-    });
+  const theBinder = binders.find(binder => {
+    return  binder.id.toString() === params.id;
+  });
+  console.log("theBinder: ", theBinder);
+  
   return (
     <div>
-      {binders.map((b) => {
-        if(b.id.toString() === params.id){ // Check for the corresponding Binder
-          console.log("This Binder's Decks: ", b.decks);
-          return (
-              <div>
-                {/* <h3>{b.name}</h3>
-                <DeckInput binder={b} />
-                  <p key={b.decks}>{b.decks}</p>
-                  {(b.decks).map((deck) => {
-                    <DeckLink key={deck.id} deck={deck} />
-                  })} */}
-              </div>
-          );
-        }
-      })}
+      <DeckInput />
+      {theBinder ? <h3>{theBinder.name}</h3> : null}
+      <ul id='deck-list'>
+        {theBinder?.decks.map(deck => {
+          console.log(deck);
+          return <DeckLink key={deck.id} binder={theBinder} deck={deck} />
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Binder;
