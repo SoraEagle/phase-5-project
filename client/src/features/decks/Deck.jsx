31ed473d901444253918d1e2590381365d7a36f5
Matthew Phillips <phillips.matthew829@gmail.com>
2023-01-31 11:59:03 -0600

    Add functionality to render the viewed Deck's name

--- client/src/features/decks/Deck.jsx
+++ client/src/features/decks/Deck.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function Deck(){
     const params = useParams();
-    const decks = useSelector((state) => state.decks.entities);
+    const binders = useSelector((state) => state.binders.entities);
+    const [thisDeck, setThisDeck] = useState(null);
 
-    const thisDeck = decks.find(deck => {
-      return deck.id.toString() === params.id;
-    });
+    console.log(params);
+
+    useEffect(() => {
+      if(binders.length >= 1){
+        const thisBinder = binders.find(binder => {return binder.id.toString() === params.binder_id;});
+        setThisDeck(thisBinder.decks.find(deck => {return deck.id.toString() === params.id}));
+        console.log(thisDeck);
+        return thisDeck;
+      }
+      return thisDeck;
+    }, [binders, params.binder_id, params.id, thisDeck]);
+
     console.log(thisDeck);
-    console.log(decks);
   return (
     // Remember to render the Flashcards
     <div>
-        {/* {thisDeck ? <h3>{thisDeck.name}</h3> : null} */}
+      <h3>I am a Deck</h3>
+        {thisDeck ? <h3>{thisDeck.name}</h3> : null}
         <ul id='card-list'></ul>
     </div>
-  )
+  );
 }
 
 export default Deck;
