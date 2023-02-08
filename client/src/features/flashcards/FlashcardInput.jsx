57ad18271e88841945c211865b3596bfcfd72b4f
Matthew Phillips <phillips.matthew829@gmail.com>
2023-02-08 10:04:59 -0600

    Add functionality to find the current Deck

--- client/src/features/flashcards/FlashcardInput.jsx
+++ client/src/features/flashcards/FlashcardInput.jsx
@@ -3,12 +3,36 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { newFlashcard } from './flashcardsSlice';
 
-function FlashcardInput({onFlashcardSubmit}){
+function FlashcardInput(){
+  const dispatch = useDispatch();
+  const params = useParams();
+  // const currentUser = useSelector(state => state.users.entities);
+  const binders = useSelector((state) => state.binders.entities);
+  // const [userId, setUserId] = useState(null);
+  // const [deckId, setDeckId] = useState(null);
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
+  
+  const thisBinder = binders.find(binder => {
+    return  binder.id.toString() === params.binder_id;
+  });
+  const thisDeck = thisBinder?.decks.find(deck => {
+    return deck.id.toString() === params.id
+  });
 
   function handleSubmit(e){
     e.preventDefault();
+    // setUserId(currentUser.id);
+    // setDeckId(thisDeck.id);
+    const flashcard = {
+      deck_id: thisDeck.id,
+      question: question,
+      answer: answer
+    }
+    console.log(flashcard);
+    dispatch(newFlashcard(flashcard));
+    setQuestion('');
+    setAnswer('');
   }
   return (
     <form id='input-form' onSubmit={handleSubmit}>
