52b866a186e6b20904f9309c1d4c3f23f0587d27
Matthew Phillips <phillips.matthew829@gmail.com>
2023-02-09 19:13:42 -0600

    Add newFlashcard extraReducers

--- client/src/features/decks/decksSlice.js
+++ client/src/features/decks/decksSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { newFlashcard } from "../flashcards/flashcardsSlice";
 import { headers } from "../../Globals";
 export const newDeck = createAsyncThunk("decks/newDeck", async (payload) => {
     return fetch(`/decks`, {
@@ -24,6 +25,23 @@ const decksSlice = createSlice({
             .addCase(newDeck.fulfilled, (state, action) => {
                 state.status = 'idle';
                 if(action.payload.errors) state.errorMessages = action.payload.errors;
+            })
+            .addCase(newFlashcard.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(newFlashcard.fulfilled, (state, action) => {
+                state.status = 'idle';
+                const flashcard = action.payload;
+                const deck = flashcard.deck;
+                if(!action.payload.errors){
+                    state.errorMessages = null;
+
+                    const thisDeck = state.entities.find(myDeck => {
+                        return myDeck.id === deck.id;
+                    });
+                    thisDeck.flashcards.push(action.payload);
+                    // Use breakpoints and console.log to double check
+                }
             })
     }
 });
