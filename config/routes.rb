9b24970bd630a1ca538f7e8110d74b6b27d54682
Matthew Phillips <phillips.matthew829@gmail.com>
2023-02-17 13:11:21 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :binders, only: [:index, :create, :update]
-  resources :decks, only: [:index, :create, :update, :destroy]
+  resources :decks, only: [:index, :create]
   resources :flashcards, only: [:index, :create, :update, :destroy]
+
+  # Sessions
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  # Users
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   
