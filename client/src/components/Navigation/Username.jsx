373c57af59335c2d6f339956b2e3009b828fba1b
Matthew Phillips <phillips.matthew829@gmail.com>
2023-03-28 16:23:42 -0500

    Create Username.jsx

---
+++ client/src/components/Navigation/Username.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function Username(){
+    const user = useSelector(state => state.users.entities);
+  return (
+    <div id='username'>
+        {(user) ? (<div>{user.username}</div>) : (null)}
+    </div>
+  )
+}
+
+export default Username;
