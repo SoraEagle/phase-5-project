c1cb995fecb55faaa41e1ac0fcdf509352b7776e
Matthew Phillips <phillips.matthew829@gmail.com>
2023-03-30 14:05:09 -0500

    Added contact info to Footer; Edited CSS

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,6 +8,9 @@ function Footer(){
         </p>
         <p>@2023 Redux React-or LLC</p>
         <p>All Rights Reserved</p>
+        <p>Created by: Matthew Phillips</p>
+        <a id='link' href='https://github.com/SoraEagle'>My GitHub</a>
+        <a id='link' href='https://www.linkedin.com/in/matthew-phillips-754a89168/'>My LinkedIn profile</a>
     </footer>
   )
 }
