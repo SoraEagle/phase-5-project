ac3830bc58da12c4fc0d5a8def4f94ae606695ef
Matthew Phillips <phillips.matthew829@gmail.com>
2023-06-02 15:27:23 -0500

    Refactor SCSS

--- client/src/features/binders/BinderLInk.jsx
+++ client/src/features/binders/BinderLInk.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 
 function BinderLink({binder}){
     return (
-    <div id='binder-link'>
-        <img id='binder-image' 
-            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR19DFLsrFz_7D9N_yGiBNugav84-FEsNjm3Q&usqp=CAU'
-            alt={binder.name}
-        />
-        <p>&nbsp;</p>
-        <Link id='linkStyles' className='link' to={`/binders/${binder.id}`}>
-            {binder.name}
-        </Link>
-    </div>
-  );
+        <div id='link'>
+            <img id='image' 
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR19DFLsrFz_7D9N_yGiBNugav84-FEsNjm3Q&usqp=CAU'
+                alt={binder.name}
+            />
+            <p>&nbsp;</p>
+            <Link id='linkStyles' to={`/binders/${binder.id}`}>
+                {binder.name}
+            </Link>
+        </div>
+    );
 }
 
 export default BinderLink;
--- client/src/features/decks/DeckLink.jsx
+++ client/src/features/decks/DeckLink.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 function DeckLink({binder, deck}){
   return (
-    <div id='deck-link'>
-        <img id='deck-image'
+    <div id='link'>
+        <img id='image'
             src='https://media.istockphoto.com/id/172162468/photo/fan-of-colored-index-cards.jpg?s=612x612&w=0&k=20&c=6RHWhNpuq4bXu3dLCnNqk28a6WejUSTWG58ahIm8DL4='
             alt={deck.name}
-         />
+        />
          <p>&nbsp;</p>
-         <Link id='linkStyles' className='link' to={`/binders/${binder.id}/decks/${deck.id}`}>
+         <Link id='linkStyles' to={`/binders/${binder.id}/decks/${deck.id}`}>
             {deck.name}
          </Link>
     </div>
